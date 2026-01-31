057a86bc77b9384049ffe75f005a2f6193f4ea77
p0rebr1k <dimadimondima28@gmail.com>
2026-02-01 01:36:41 +0300

    Лаб3

--- task1.py
+++ task1.py
@@ -1,15 +1,15 @@
-money_capital = 20000  # Подушка безопасности
-salary = 5000  # Ежемесячная зарплата
-spend = 6000  # Траты за первый месяц
-increase = 0.05  # Ежемесячный рост цен
+# TODO Напишите функцию для поиска индекса товара
+def find_item_index(items, target_item):
+    if target_item in items:
+        return items.index(target_item)
+    else:
+        return None
 
-# TODO Посчитайте количество  месяцев, которое можно протянуть без долгов
-month = 0
-while money_capital >= 0:
-    if spend > money_capital + salary:
-        break
-    money_capital = money_capital + salary - spend
-    month += 1
-    spend *= 1 + increase
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
 
-print("Количество месяцев, которое можно протянуть без долгов:", month)
+for find_item in ['банан', 'груша', 'персик']:
+    index_item =find_item_index(items_list, find_item)  # TODO Вызовите функцию, что получить индекс товара
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
